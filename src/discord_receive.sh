3e36fc81a22df283bbd0adfdb40a5c98e70a0dbd
GGomsi <jsuhak@naver.com>
2024-02-13 23:43:08 +0900

    add discord

---
+++ src/discord_receive.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python /workspace/worker/discord/receiver.py --params /workspace/worker/discord/sender_params.json --local_path '/workspace/worker/discord/images'
---
+++ src/discord_send.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python /workspace/worker/discord/sender.py --params /workspace/worker/discord/sender_params.json --prompt "$@"
